f421ad7767538f2e0e92f5fe5c9d63cddac444fc
Matt Doering <ablueplatypus21@gmail.com>
2018-11-20 19:21:28 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
+class Book
 
+attr_accessor :author, :page_count, :genre
+attr_reader :title
 
+def initialize(title)
+ @title = title
+end
+
+# def title #<-- This was not needed because it is in our reader!!
+#  @title
+# end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+#Book.new("And Then There Were None")
+
+end #end of class
+
+#||||| DRY||| DONT REPAT YOURSELF!!!|||||
+# def author
+#   @author
+# end
+#
+# def author=(author)
+#   @author = author
+# end
+
+# def page_count
+#   @page
+# end
+#
+# def page_count=(page_count)
+#   @page = page_count
+# end
+#
+# def genre
+#   @genre
+# end
+#
+# def genre=(genre)
+#   @genre = genre
+# end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe
+
+attr_accessor :color, :size, :material, :condition #can we change these attriabutes? The answer should be YES
+attr_reader :brand
+
+def initialize(brand) #I have initialized a brand for class Shoe.
+  @brand = brand
+end
+
+def cobble
+ #@condition = "new"
+ puts "Your shoe is as good as new!"
+ self.condition = "new"
+
+end
+
+# def brand #I can access the brand of the shoe <-- not needed it is in the attr_reader
+#   @brand
+# end
+
+end #end of class
